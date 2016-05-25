27e74ab22ca54633f50ef715b535bb06ad08e15c
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-05-25 10:08:22 +0200

    Creating tar ball

--- build.sh
+++ build.sh
@@ -4,24 +4,31 @@ if [[ -z ${GOPATH} ]]; then
 fi
 
 rm -rf target
+buildroot=target/writer-toolbox
 
-echo -n "Installing dependencies ..."
+echo -n "Installing dependencies ... "
 go get 
 echo "done"
 
-echo -n "Compiling OS X binary ..."
-mkdir -p target/osx
-go build -o target/osx/writer-tool
+echo -n "Compiling OS X binary ... "
+mkdir -p ${buildroot}/osx
+go build -o ${buildroot}/osx/writer-tool
 echo "done"
 
-echo -n "Compiling amd64 Linux binary ..."
-mkdir -p target/linux/amd64
-env GOOS=linux GOARCH=amd64 go build -o target/linux/amd64/writer-tool
+echo -n "Compiling amd64 Linux binary ... "
+mkdir -p ${buildroot}/linux/amd64
+env GOOS=linux GOARCH=amd64 go build -o ${buildroot}/linux/amd64/writer-tool
 echo "done"
 
-echo -n "Compiling 386 Linux binary ..."
-mkdir -p target/linux/386
-env GOOS=linux GOARCH=386 go build -o target/linux/386/writer-tool
+echo -n "Compiling 386 Linux binary ... "
+mkdir -p ${buildroot}/linux/386
+env GOOS=linux GOARCH=386 go build -o ${buildroot}/linux/386/writer-tool
 echo "done"
 
-echo "Binaries are found in ./target"
+echo -n "Creating tarball ... "
+tar zcf writer-toolbox.tgz target
+echo "done"
+
+mv writer-toolbox.tgz target
+
+echo "Tarball is placed in ./target"

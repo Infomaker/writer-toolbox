fc9349646e4044ce906bb0cb9a991ed753cfd795
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-06-23 13:08:37 +0200

    Removing output from docker build

--- docker.sh
+++ docker.sh
@@ -11,19 +11,9 @@ rm -rf target
 mkdir target
 
 echo -n "Building writer toolbox ... "
-docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e GOOS=linux -e GOARCH=amd64 -e CGO_ENABLED=0 golang:1.6 bash -c "go get -d -v; go build -ldflags '-s' -v -o target/writer-tool"
+docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e GOOS=linux -e GOARCH=amd64 -e CGO_ENABLED=0 golang:1.6 bash -c "go get -d ; go build -ldflags '-s' -o target/writer-tool"
 echo "done"
 
 cp Dockerfile target
 
-echo -n "Creating docker image ... "
-docker build --no-cache=true -t  685070497634.dkr.ecr.eu-west-1.amazonaws.com/writer-tool:$1 target
-
-echo -n "Logging in to docker ... "
-eval $(aws ecr --profile im-docker-push --region eu-west-1 get-login --registry-ids 685070497634)
-echo "done"
-
-echo -n "Pusing to registry"
-docker push 685070497634.dkr.ecr.eu-west-1.amazonaws.com/writer-tool:$1
-echo "done"
 

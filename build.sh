9ebbd612790ee13c110710f87c1cd9bfb8d5e60c
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-07-05 16:01:13 +0200

    Removed linux targets

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ mkdir -p ${buildroot}/osx
 go build -o ${buildroot}/osx/writer-tool
 echo "done"
 
-echo -n "Compiling amd64 Linux binary ... "
-mkdir -p ${buildroot}/linux/amd64
-env GOOS=linux GOARCH=amd64 go build -o ${buildroot}/linux/amd64/writer-tool
-echo "done"
+#echo -n "Compiling amd64 Linux binary ... "
+#mkdir -p ${buildroot}/linux/amd64
+#env GOOS=linux GOARCH=amd64 go build -o ${buildroot}/linux/amd64/writer-tool
+#echo "done"
 
-echo -n "Compiling 386 Linux binary ... "
-mkdir -p ${buildroot}/linux/386
-env GOOS=linux GOARCH=386 go build -o ${buildroot}/linux/386/writer-tool
-echo "done"
+#echo -n "Compiling 386 Linux binary ... "
+#mkdir -p ${buildroot}/linux/386
+#env GOOS=linux GOARCH=386 go build -o ${buildroot}/linux/386/writer-tool
+#echo "done"
 
 echo -n "Creating tarball ... "
 tar zcf writer-toolbox.tgz target

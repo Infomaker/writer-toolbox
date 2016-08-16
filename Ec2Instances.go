5d01327cb508b710a7ed497f9b5023a6a8da2ff0
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-08-16 13:31:26 +0200

    Added getEntity command Added verbosity levels to listLoadBalancers

--- Ec2Instances.go
+++ Ec2Instances.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"regexp"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"fmt"
 	"os"
 	"github.com/aws/aws-sdk-go/service/elb"
+	"net/http"
+	"io/ioutil"
 )
-
-func Ec2Name(a *string) string {
-	re := regexp.MustCompile("\\w+:\\w+:\\w+:[\\w-]+:\\d+:\\w+\\/(.+)")
-
-	res := re.FindAllStringSubmatch(*a, -1);
-
-	if res == nil {
-		return "No match for " + *a;
-	}
-
-	return res[0][1]
-}
 
 func _listEc2Instances() *ec2.DescribeInstancesOutput {
 	svc := ec2.New(session.New(), _getAwsConfig())
@@ -50,7 +38,7 @@ func ListEc2Instances() {
 			if (*instance.State.Name == "running") {
 				if (verboseLevel == 2) {
 					fmt.Printf("%s (%s): %s, %s \n", *instance.InstanceId, *instance.PublicIpAddress, _getName(instance.Tags), *instance.State.Name)
-				} else if (verboseLevel == 1){
+				} else if (verboseLevel == 1) {
 					fmt.Println(_getName(instance.Tags))
 				} else {
 					fmt.Println(*instance.InstanceId)
@@ -60,6 +48,45 @@ func ListEc2Instances() {
 	}
 }
 
+func GetEntity(loadBalancerId, entityId string) {
+	resp := _listLoadBalancers();
+
+	host := ""
+
+	for i := 0; i < len(resp.LoadBalancerDescriptions); i++ {
+		if (loadBalancerId == *resp.LoadBalancerDescriptions[i].LoadBalancerName) {
+			host = *resp.LoadBalancerDescriptions[i].DNSName
+		}
+	}
+
+	if (host != "") {
+		url := "http://" + host + "/api/newsItem/" + entityId
+		if (verboseLevel > 0) {
+			fmt.Printf("Fetching from url [%s]\n", url);
+		}
+		resp, err := http.Get(url);
+
+		if (err != nil) {
+			errState(err.Error())
+		}
+
+		if (resp.StatusCode == 200) {
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if (err != nil) {
+				errState(err.Error());
+			}
+			fmt.Println(string(body));
+		} else {
+			fmt.Println(resp.StatusCode);
+		}
+
+		return;
+	} else {
+		errState("No host found for load balancer: " + loadBalancer)
+	}
+
+}
 
 func _listLoadBalancers() *elb.DescribeLoadBalancersOutput {
 	svc := elb.New(session.New(), _getAwsConfig())
@@ -82,14 +109,20 @@ func ListLoadBalancers() {
 	for i := 0; i < len(resp.LoadBalancerDescriptions); i++ {
 		loadBalancer := resp.LoadBalancerDescriptions[i]
 		instances := _listEc2Instances();
-		fmt.Printf("%s (%s)\n", *loadBalancer.LoadBalancerName, *loadBalancer.DNSName)
-		for j:= 0; j < len(loadBalancer.Instances); j++ {
-			instanceItem := loadBalancer.Instances[j]
-			for k := 0; k < len(instances.Reservations); k++ {
-				//fmt.Printf("Iterating over reservation %d/%d containing %d instances\n", k, len(instances.Reservations), len(instances.Reservations[k].Instances))
-				instance := getInstanceForId(instances.Reservations[k].Instances, *instanceItem.InstanceId)
-				if (instance != nil) {
-					fmt.Printf("  * %s (%s): %s, %s\n", *instance.InstanceId, *instance.PublicIpAddress, _getName(instance.Tags), *instance.State.Name)
+		if (verboseLevel == 0) {
+			fmt.Println(*loadBalancer.LoadBalancerName)
+		} else if (verboseLevel == 1) {
+			fmt.Println(*loadBalancer.DNSName)
+		} else {
+			fmt.Printf("%s (%s)\n", *loadBalancer.LoadBalancerName, *loadBalancer.DNSName)
+			for j := 0; j < len(loadBalancer.Instances); j++ {
+				instanceItem := loadBalancer.Instances[j]
+				for k := 0; k < len(instances.Reservations); k++ {
+					//fmt.Printf("Iterating over reservation %d/%d containing %d instances\n", k, len(instances.Reservations), len(instances.Reservations[k].Instances))
+					instance := getInstanceForId(instances.Reservations[k].Instances, *instanceItem.InstanceId)
+					if (instance != nil) {
+						fmt.Printf("  * %s (%s): %s, %s\n", *instance.InstanceId, *instance.PublicIpAddress, _getName(instance.Tags), *instance.State.Name)
+					}
 				}
 			}
 		}

faf5f39274dc166c455a880dc46b6ce6bb8e6d09
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-11-02 16:20:35 +0100

    Added functionality to deploy lambda code and update alias pointer

--- Lambda.go
+++ Lambda.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"strconv"
 )
 
 func _getLambdaFunctionAliasInfo(functionName, alias string) *lambda.AliasConfiguration {
@@ -38,7 +39,68 @@ func _getLambdaFunctionInfo(functionName, qualifier string) *lambda.FunctionConf
 		errState(err.Error())
 	}
 
-	return resp;
+	return resp
+}
+
+
+func _listLambdaFunctions() *lambda.ListFunctionsOutput {
+	svc := lambda.New(session.New(), _getAwsConfig())
+
+	params := &lambda.ListFunctionsInput{
+
+	}
+
+	resp, err := svc.ListFunctions(params)
+
+	if (err != nil) {
+		errState(err.Error())
+	}
+
+	return resp
+}
+
+
+func _deployLambdaFunction(functionName, bucket, filename, alias string, publish bool) {
+	svc := lambda.New(session.New(), _getAwsConfig())
+
+	params := &lambda.UpdateFunctionCodeInput{
+		FunctionName: aws.String(functionName),
+		S3Bucket: aws.String(bucket),
+		S3Key: aws.String(filename),
+		Publish: aws.Bool(publish),
+	}
+
+	result, err  := svc.UpdateFunctionCode(params)
+
+	if err != nil {
+		errState(err.Error())
+	}
+
+	if publish {
+
+		params := &lambda.UpdateAliasInput{
+			FunctionName: aws.String(functionName),
+			FunctionVersion: aws.String(*result.Version),
+			Name: aws.String(alias),
+		}
+
+		_, err := svc.UpdateAlias(params)
+
+		if (err != nil) {
+			errState(err.Error())
+		}
+	}
+}
+
+
+func DeployLambdaFunction(functionName, bucket, filename, alias, publish string) {
+	doPublish, err := strconv.ParseBool(publish)
+
+	if (err != nil) {
+		doPublish = false;
+	}
+
+	_deployLambdaFunction(functionName, bucket, filename, alias, doPublish)
 }
 
 func GetLambdaFunctionAliasInfo(functionName, alias string) {
@@ -46,9 +108,19 @@ func GetLambdaFunctionAliasInfo(functionName, alias string) {
 
 	functionInfo := _getLambdaFunctionInfo(functionName, *aliasInfo.FunctionVersion)
 
-	fmt.Println(*functionInfo.Description);
+	fmt.Println(*functionInfo.Version, ": ", *functionInfo.Description);
 }
 
 func GetLambdaFunctionInfo(functionName string) {
 	fmt.Println(_getLambdaFunctionInfo(functionName, "$LATEST"))
+}
+
+
+func ListLambdaFunctions() {
+
+	result := _listLambdaFunctions()
+
+	for i := 0; i < len(result.Functions); i++ {
+		fmt.Println(*result.Functions[i].FunctionName)
+	}
 }
